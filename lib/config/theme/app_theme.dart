24d58b9ad9339c608bf02725cdd94c9c6249cce7
Isj-Code <isj.code@gmail.com>
2024-05-09 16:41:55 +0200

    First Commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF5C11D4);
+const List<Color> _colorsTheme = [];
+
+class AppTheme {
+  ThemeData theme() {
+    return ThemeData();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().theme(),
+      title: 'Yes No App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Yes No App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: FilledButton(
+            onPressed: () {},
+            child: const Text('ClicKMe'),
+          ),
+        ),
+      ),
+    );
+  }
+}
